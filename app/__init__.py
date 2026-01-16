a576555f506b8e20b49ff04ad8774990e5a040f6
pzpluz <mxhsy_8301@163.com>
2026-01-16 21:10:27 +0800

    chapter04: databases finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pymysql
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-print('等会谁(哪个包或模块)在使用我: ', __name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+pymysql.install_as_MySQLdb()
 
 
-from app import routes
+# 导入一个新模块 models, 用来定义数据库的结构
+from app import routes, models
--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 import os
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:199716128@localhost/microblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
